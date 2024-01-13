6fffc55ecec801effd1a04a08d977116678d6ac5
Min Thant Mg <minthant180@gmail.com>
2024-01-13 16:10:16 +0630

    fixed text size

--- src/app/heroSection/hero.js
+++ src/app/heroSection/hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
                 <div className={`relative -mt-28 -mr-36 ml-24 ${styles.floatingImage}`}>
                     <Image src={bitcoin} alt=""/>
                 </div>
-                <div className="text-[130px] mt-8 space-x-6 font-mono font-bold leading-tight text-center">
+                <div className="text-[110px] mt-12 space-x-6 font-mono font-bold leading-tight text-center">
                     TRACK AND TRADE <span className=" bg-gradient-to-r from-blue-700 to-purple-900 bg-clip-text text-transparent">CRYPTO CURRENCIES</span>
                 </div>
                 <div className={`relative -mt-28 -ml-36 mr-28 ${styles.floatingImage}`}>
