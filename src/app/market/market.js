88f4acdb7ca7bafd8bfeb5c0e46faf351254505b
Min Thant Mg <minthant180@gmail.com>
2024-01-27 15:16:49 +0630

    update for mobile ui

--- src/app/market/market.js
+++ src/app/market/market.js
@@ -18,7 +18,7 @@ const Market = () => {
 
     return (
         <div className="bg-black text-white">
-            <div className="font-bold text-3xl ml-28 sm:pt-16">
+            <div className="font-bold text-3xl sm:ml-28 sm:pt-16 ml-20">
                 Market Update
             </div>
             {isError && (
@@ -37,17 +37,17 @@ const Market = () => {
             )}
             <div className="">
                 <div
-                    className="mt-16 flex justify-between p-4 rounded-t-lg bg-gradient-to-r from-blue-700 to-purple-900 ml-24 text-white mr-20">
-                    <div className="flex items-start text-2xl font-bold w-1/4">
+                    className="mt-16 flex justify-between sm:p-4 rounded-t-lg bg-gradient-to-r from-blue-700 to-purple-900 sm:ml-24 text-white sm:mr-20">
+                    <div className="flex items-start sm:text-2xl font-bold sm:w-1/4">
                         Coin
                     </div>
-                    <div className="text-2xl font-bold text-end mr-2 w-1/4">
+                    <div className="sm:text-2xl font-bold sm:w-1/4">
                         Price
                     </div>
-                    <div className="text-2xl font-bold text-end w-1/4">
+                    <div className="sm:text-2xl font-bold sm:w-1/4 text-end">
                         24h Change
                     </div>
-                    <div className="text-2xl font-bold w-1/4 text-end">
+                    <div className="sm:text-2xl font-bold sm:w-1/4 text-end">
                         Market Cap
                     </div>
                 </div>
@@ -57,24 +57,24 @@ const Market = () => {
                             <div key={index} >
                                 <Link legacyBehavior={true} href={`/detail/${coin.id}`}>
                                     <a>
-                                        <div className="flex justify-between mt-4 pb-4 text-2xl font-bold border-b-2 ml-24 mr-20">
-                                            <div className="flex w-1/4">
+                                        <div className="flex justify-between sm:mt-4 sm:pb-4 sm:text-2xl font-bold border-b-2 sm:ml-24 sm:mr-20">
+                                            <div className="flex sm:w-1/4">
                                                 <img src={coin.image} alt="" className="w-12 h-12"/>
                                                 <div className="flex justify-center items-center ml-4">
                                                     {coin.name}
                                                 </div>
                                             </div>
-                                            <div className="text-end text-2xl font-bold mt-2 w-1/4">
+                                            <div className="text-end text-2xl font-bold mt-2 sm:w-1/4">
                                                 $ {coin.current_price.toLocaleString(undefined, {
                                                 minimumFractionDigits: 2,
                                                 maximumFractionDigits: 2
                                             })}
                                             </div>
                                             <div
-                                                className={`mt-2 w-1/4 text-end ${coin.price_change_percentage_24h > 0 ? 'text-green-500' : 'text-red-500'}`}>
+                                                className={`mt-2 sm:w-1/4 text-end ${coin.price_change_percentage_24h > 0 ? 'text-green-500' : 'text-red-500'}`}>
                                                 {coin.price_change_percentage_24h.toFixed(2)}%
                                             </div>
-                                            <div className="mt-2 w-1/4 text-end mr-2">
+                                            <div className="mt-2 sm:w-1/4 text-end mr-2">
                                                 $ {coin.market_cap.toLocaleString()}
                                             </div>
                                         </div>
